5384bc9d101ebe22a77b6ed71f70bd9f9440f26f
marvellouschinedu9-eng <marvellouschinedu9@gmail.com>
2026-02-23 11:07:58 +0100

    Chapter 2

---
+++ JavaOutput.java
@@ -0,0 +1,14 @@
+public class JavaOutput{
+	
+public static void main(String[] arg) {
+	System.out.println("Hello world, this is my frist Java Program");
+	System.out.println("Learning Java Is Fun");
+	
+	
+	//Using the print method
+	system.ou.print("Do You Love Learning Java? ");
+	system.out.print("Yes I Love Learning Java ");
+	
+	//using the printf methoth
+	System.out.printf("My name is Mercy Ben, I am %d Years old", 25)
+}
